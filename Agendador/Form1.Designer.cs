9ecb744006ac7ce45d08b63d0a5b3906b4980753
Rodrigo Escobar <rodescobar@gmail.com>
2021-05-12 10:59:20 -0300

    Adicionar arquivos de projeto.

---
+++ Agendador/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Agendador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Agendador.Properties.Resources.discord11;
+            this.pictureBox1.Image = global::Agendador.Properties.Resources.discord1;
+            this.pictureBox1.Location = new System.Drawing.Point(252, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Image = global::Agendador.Properties.Resources.galocego1;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(237, 68);
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(344, 101);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "<GaloCego Scripts/>";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Agendador/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace Agendador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://galocego.vercel.app");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.com/channels/841140239783165983");
+        }
+
+        class Configuracoes
+        {
+            public string source { get; set; }
+            public string destino { get; set; }
+            public string usuario { get; set; }
+            public string senha { get; set; }
+            public string banco { get; set; }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer t = new Timer();
+            t.Interval = 5000;
+            t.Tick += new EventHandler(Fechar);
+            t.Start();
+        }
+
+        private void Fechar(object sender, EventArgs e)
+        {
+            string path = "conf.json";
+            if (File.Exists(path))
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string json = r.ReadToEnd();
+                    Configuracoes items = JsonConvert.DeserializeObject<Configuracoes>(json);
+
+                    string comando = items.source + @"\mysqldump.exe";
+                    comando += " -u " + items.usuario;
+
+                    if (items.senha != "")
+                    {
+                        comando += " -p" + items.senha;
+                    }
+
+                    DateTime data = DateTime.Now;
+                    string texto = data.ToString("yyyyMMddHHmmss");
+
+                    comando += " -h localhost " + items.banco + @" > " + items.destino + @"\backup" + texto + ".sql";
+
+                    Process p = new Process();
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    info.FileName = "cmd.exe";
+                    info.RedirectStandardInput = true;
+                    info.UseShellExecute = false;
+
+                    p.StartInfo = info;
+                    p.Start();
+
+                    using (StreamWriter sw = p.StandardInput)
+                    {
+                        if (sw.BaseStream.CanWrite)
+                        {
+                            sw.WriteLine(comando);
+                        }
+                    }
+                }
+                Thread.Sleep(30000);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Não foi encontrado o banco ou o MySQL, reveja o arquivo de configuração.");
+            }
+        }
+    }
+}
